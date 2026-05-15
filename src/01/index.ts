b96cfe353b3a88c29e4854132fffa74a7b85b13f
13inuo0 <13inu.o0@gmail.com>
2026-05-15 19:50:21 +0900

    동일한 내용의 타입을 중복작성 시 가독성과 유지보수가 떨어진다.

---
+++ src/01/index.ts
@@ -0,0 +1,18 @@
+const user1: { name: string; age: number; gender: "male" | "female" } = {
+  name: "철수",
+  age: 20,
+  gender: "male",
+};
+const user2: { name: string; age: number; gender: "male" | "female" } = {
+  name: "유리",
+  age: 22,
+  gender: "female",
+};
+const user3: { name: string; age: number; gender: "male" | "female" } = {
+  name: "훈이",
+  age: 25,
+  gender: "male",
+};
+// 타입유형이 동일. 중복으로 작성되어 가독성이 떨어진다.
+// 객체의 속성이 변경되면 타입 객체의 속성명도 같이 변경해줘야 하기때문에 유지보수면에 불리.
+// 때문에 확장성을 고려하여 타입을 정의하는 방법인 인터페이스를 사용
