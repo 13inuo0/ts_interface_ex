1219ed9ffbc9b76b5c562e3a70087e4bdfe69c35
13inuo0 <13inu.o0@gmail.com>
2026-05-17 18:29:19 +0900

    모든 키와 값의 타입이 같은경우 인덱스 시그니처 사용가능

--- src/01/index.ts
+++ src/01/index.ts
@@ -5,9 +5,7 @@
 //   gender: "male" | "female";
 // }
 
-import type { User } from "../types/interface_ex";
-
-
+import type { User, User4 } from "../types/interface_ex.ts";
 
 const user1: User = {
   name: "철수",
@@ -27,3 +25,10 @@ const user3: User = {
 // 타입유형이 동일. 중복으로 작성되어 가독성이 떨어진다.
 // 객체의 속성이 변경되면 타입 객체의 속성명도 같이 변경해줘야 하기때문에 유지보수면에 불리.
 // 때문에 확장성을 고려하여 타입을 정의하는 방법인 인터페이스를 사용
+
+// 인덱스 시그니처 사용
+const user4: User4 = {
+  name: "철수",
+  gender: "male",
+  address: "seoul",
+};
--- src/types/interface_ex.ts
+++ src/types/interface_ex.ts
@@ -11,4 +11,12 @@ interface User2 {
 interface Greet {
   (name: string): string;
 }
-const greet:Greet = (name)=>`Hello, ${name}`
+const greet: Greet = (name) => `Hello, ${name}`;
+
+// 키와 값이 모두 문자열인 경우 인덱스 시그니처 사용가능.
+export interface User4 {
+  // name: string;
+  // gender: string;
+  // address: string;
+  [key: string]: string;
+}
