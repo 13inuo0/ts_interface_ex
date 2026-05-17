c36200cb14d67bc5f0dcfc127a5ba56254a182f9
13inuo0 <13inu.o0@gmail.com>
2026-05-17 18:34:51 +0900

    인터페이스 병합

--- src/types/interface_ex.ts
+++ src/types/interface_ex.ts
@@ -18,5 +18,19 @@ export interface User4 {
   // name: string;
   // gender: string;
   // address: string;
-  [key: string]: string;
+  [key: string]: string | number;
 }
+
+// 인터페이스 병합
+// 같은 이름의 인터페이스는 자동으로 합쳐진다.
+interface Same {
+  name: string;
+}
+interface Same {
+  age: number;
+}
+
+let user5: Same = {
+  name: "철수",
+  age: 20,
+}; //오류 없음
